4317d6e9d3f2adbbece498d475402620fc03e5bc
Harshit Hawaldar <Harshit.Hawaldar@gds.ey.com>
2019-10-20 21:51:42 +0530

    it begins2

---
+++ src/main/java/Cloud/Test/AzureappserviceApplication.java
@@ -0,0 +1,13 @@
+package Cloud.Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureappserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureappserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Cloud/Test/AzureappserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package Cloud.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureappserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
